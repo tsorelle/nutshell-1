7256d3aeff36bdf3f0dbba28c800936151374642
Terry SoRelle <tls@2quakers.net>
2022-05-14 16:11:24 -0500

    fixed issues with home paged

--- web.root/nutshell/src/cms/routing/RouteFinder.php
+++ web.root/nutshell/src/cms/routing/RouteFinder.php
@@ -5,49 +5,38 @@ namespace Nutshell\cms;
 class RouteFinder
 {
     public static $matched = null;
-    public static  function match ($uri)
-    {
-        $path = '/';
-        $argValues = [];
-        $configuration = [];
 
+    public static function match($uri)
+    {
         if ($uri === '') {
             $uri = 'home';
         }
-        else {
-            $config = parse_ini_file(DIR_CONFIG_SITE.'/routing.ini',true);
-            foreach ($config as $matchPath => $values) {
-                if (strpos($uri, $matchPath) === 0) {
-                    if ($uri !== 'home') {
-                        $matchParts = explode('/', $matchPath);
-                        $matchCount = count($matchParts);
-                        $pathParts = explode('/', $uri);
-                        for ($i = 0; $i < $matchCount; $i++) {
-                            if ($pathParts[$i] !== $matchParts[$i]) {
-                                return false;
-                            }
-                        }
-                        $configuration = $values;
-                        $pathCount = count($pathParts);
-                        $argCount = $pathCount - $matchCount;
-                        if ($argCount > 0) {
-                            $argValues = array_splice($pathParts, $matchCount);
-                        }
+        $config = parse_ini_file(DIR_CONFIG_SITE . '/routing.ini', true);
+        foreach ($config as $matchPath => $values) {
+            if (strpos($uri, $matchPath) === 0) {
+                $matchParts = explode('/', $matchPath);
+                $matchCount = count($matchParts);
+                $pathParts = explode('/', $uri);
+                for ($i = 0; $i < $matchCount; $i++) {
+                    if ($pathParts[$i] !== $matchParts[$i]) {
+                        return false;
                     }
-
-                    $configuration['uri'] = $uri;
-                    $configuration['path'] = $matchPath;
-                    $configuration['argValues'] = $argValues;
-                    self::$matched = $configuration;
-
-                    return true;
                 }
+                $configuration = $values;
+                $pathCount = count($pathParts);
+                $argCount = $pathCount - $matchCount;
+                if ($argCount > 0) {
+                    $argValues = array_splice($pathParts, $matchCount);
+                }
+
+                $configuration['uri'] = $uri;
+                $configuration['path'] = $matchPath;
+                $configuration['argValues'] = $argValues ?? [];
+                self::$matched = $configuration;
+
+                return true;
             }
-
-
-
         }
-
         return false;
     }
 }
